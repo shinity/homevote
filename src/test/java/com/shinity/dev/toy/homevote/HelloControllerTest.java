13ba43eb406c45b9d5d2c52568a9fe54b35d67bf
devshin <shinity7078@gmail.com>
2022-05-21 15:59:12 +0900

    HelloControllerTest.java 컨트롤러 테스트 클래스 작성

--- src/test/java/com/shinity/dev/toy/homevote/HelloControllerTest.java
+++ src/test/java/com/shinity/dev/toy/homevote/HelloControllerTest.java
@@ -1,11 +1,21 @@
 package com.shinity.dev.toy.homevote;
 
+import com.shinity.dev.toy.homevote.web.HelloController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
@@ -15,8 +25,8 @@ public class HelloControllerTest {
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
-        /*mvc.perform(get("/hello"))
+        mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(hello));*/
+                .andExpect(MockMvcResultMatchers.content().string(hello));
     }
 }
