39a65ea8a62deb7a0a0a5240f727d7f5f821f4cc
devshin <shinity7078@gmail.com>
2022-05-27 20:42:46 +0900

    jpa crud service, api, view, controller 구성 oauth 설정 진행중

--- src/main/java/com/shinity/dev/toy/homevote/service/posts/PostsService.java
+++ src/main/java/com/shinity/dev/toy/homevote/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.shinity.dev.toy.homevote.service.posts;
 
 import com.shinity.dev.toy.homevote.domain.posts.Posts;
 import com.shinity.dev.toy.homevote.domain.posts.PostsRepository;
+import com.shinity.dev.toy.homevote.web.dto.PostsListResponseDto;
 import com.shinity.dev.toy.homevote.web.dto.PostsResponseDto;
 import com.shinity.dev.toy.homevote.web.dto.PostsSaveRequestDto;
 import com.shinity.dev.toy.homevote.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/shinity/dev/toy/homevote/web/dto/PostsListResponseDto.java
@@ -0,0 +1,23 @@
+package com.shinity.dev.toy.homevote.web.dto;
+
+import com.shinity.dev.toy.homevote.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
